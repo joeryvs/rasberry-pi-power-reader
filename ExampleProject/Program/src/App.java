a089de73ef22bb47526856f6e977dc8003f03826
Joery <100026928+joeryvs@users.noreply.github.com>
2025-08-29 15:37:43 +0200

    factorial example

--- ExampleProject/Program/src/App.java
+++ ExampleProject/Program/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World ! again");
+        System.out.println(Factorial((0)));
+        System.out.println(Factorial((1)));
+        System.out.println(Factorial((2)));
+        System.out.println(Factorial((3)));
+        System.out.println(Factorial((4)));
+    }
+
+    private static int Factorial(int n){
+
+        int res = 1;
+        for (int i = 1; i <= n; i++) {
+            res = res * i;
+        }
+        return  res;
     }
 }
